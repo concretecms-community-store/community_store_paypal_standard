2fbe3669dd50bd98fdc123e345116bfeb8090927
Ryan Hewitt <ryan@mesuva.com.au>
2019-01-16 08:53:12 +0000

    Increment package number

--- controller.php
+++ controller.php
@@ -11,7 +11,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'community_store_paypal_standard';
     protected $appVersionRequired = '5.7.2';
-    protected $pkgVersion = '1.1';
+    protected $pkgVersion = '1.2';
 
     public function getPackageDescription()
     {
