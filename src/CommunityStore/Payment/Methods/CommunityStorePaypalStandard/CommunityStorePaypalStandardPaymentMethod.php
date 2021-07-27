fee97d7b3610a3560ddc2c0c99b26e4feb153e4a
Ryan Hewitt <ryan@mesuva.com.au>
2021-07-27 11:02:00 +0100

    - improve handling of unrelated IPN messages, handle refunds correctly

--- src/CommunityStore/Payment/Methods/CommunityStorePaypalStandard/CommunityStorePaypalStandardPaymentMethod.php
+++ src/CommunityStore/Payment/Methods/CommunityStorePaypalStandard/CommunityStorePaypalStandardPaymentMethod.php
@@ -140,98 +140,107 @@ class CommunityStorePaypalStandardPaymentMethod extends StorePaymentMethod
     }
     public static function validateCompletion()
     {
-        // Read POST data
-        // reading posted data directly from $_POST causes serialization
-        // issues with array data in POST. Reading raw POST data from input stream instead.
-        $raw_post_data = file_get_contents('php://input');
-        $raw_post_array = explode('&', $raw_post_data);
-        $myPost = array();
-        foreach ($raw_post_array as $keyval) {
-            $keyval = explode ('=', $keyval);
-            if (count($keyval) == 2)
-                $myPost[$keyval[0]] = urldecode($keyval[1]);
-        }
-        // read the post from PayPal system and add 'cmd'
-        $req = 'cmd=_notify-validate';
-        if(function_exists('get_magic_quotes_gpc')) {
-            $get_magic_quotes_exists = true;
-        }
-        foreach ($myPost as $key => $value) {
-            if($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) {
-                $value = urlencode(stripslashes($value));
-            } else {
-                $value = urlencode($value);
+        if (isset($_POST['invoice']) && $_POST['invoice']) {
+            // Read POST data
+            // reading posted data directly from $_POST causes serialization
+            // issues with array data in POST. Reading raw POST data from input stream instead.
+            $raw_post_data = file_get_contents('php://input');
+            $raw_post_array = explode('&', $raw_post_data);
+            $myPost = [];
+            foreach ($raw_post_array as $keyval) {
+                $keyval = explode('=', $keyval);
+                if (count($keyval) == 2)
+                    $myPost[$keyval[0]] = urldecode($keyval[1]);
             }
-            $req .= "&$key=$value";
-        }
-        // Post IPN data back to PayPal to validate the IPN data is genuine
-        // Without this step anyone can fake IPN data
-        if(Config::get('community_store_paypal_standard.paypalTestMode') == true) {
-            $paypal_url = "https://ipnpb.sandbox.paypal.com/cgi-bin/webscr";
-        } else {
-            $paypal_url = "https://ipnpb.paypal.com/cgi-bin/webscr";
-        }
-        $ch = curl_init($paypal_url);
-        if ($ch == FALSE) {
-            return FALSE;
-        }
-        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-        curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+            // read the post from PayPal system and add 'cmd'
+            $req = 'cmd=_notify-validate';
+            if (function_exists('get_magic_quotes_gpc')) {
+                $get_magic_quotes_exists = true;
+            }
+            foreach ($myPost as $key => $value) {
+                if ($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) {
+                    $value = urlencode(stripslashes($value));
+                } else {
+                    $value = urlencode($value);
+                }
+                $req .= "&$key=$value";
+            }
+            // Post IPN data back to PayPal to validate the IPN data is genuine
+            // Without this step anyone can fake IPN data
+            if (Config::get('community_store_paypal_standard.paypalTestMode') == true) {
+                $paypal_url = "https://ipnpb.sandbox.paypal.com/cgi-bin/webscr";
+            } else {
+                $paypal_url = "https://ipnpb.paypal.com/cgi-bin/webscr";
+            }
+            $ch = curl_init($paypal_url);
+            if ($ch == FALSE) {
+                return FALSE;
+            }
+            curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+            curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
 
-        // CONFIG: Optional proxy configuration
-        //curl_setopt($ch, CURLOPT_PROXY, $proxy);
-        //curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1);
-        // Set TCP timeout to 30 seconds
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+            // CONFIG: Optional proxy configuration
+            //curl_setopt($ch, CURLOPT_PROXY, $proxy);
+            //curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1);
+            // Set TCP timeout to 30 seconds
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'User-Agent: PHP-IPN-Verification-Script',
-            'Connection: Close',
-        ));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                'User-Agent: PHP-IPN-Verification-Script',
+                'Connection: Close',
+            ]);
 
-        // CONFIG: Please download 'cacert.pem' from "http://curl.haxx.se/docs/caextract.html" and set the directory path
-        // of the certificate as shown below. Ensure the file is readable by the webserver.
-        // This is mandatory for some environments.
-        //$cert = __DIR__ . "./cacert.pem";
-        //curl_setopt($ch, CURLOPT_CAINFO, $cert);
+            // CONFIG: Please download 'cacert.pem' from "http://curl.haxx.se/docs/caextract.html" and set the directory path
+            // of the certificate as shown below. Ensure the file is readable by the webserver.
+            // This is mandatory for some environments.
+            //$cert = __DIR__ . "./cacert.pem";
+            //curl_setopt($ch, CURLOPT_CAINFO, $cert);
 
-        $res = curl_exec($ch);
-        if (curl_errno($ch) != 0) // cURL error
-        {
-            Log::addError("Can't connect to PayPal to validate IPN message: " . curl_error($ch));
-            curl_close($ch);
-            exit;
-        } else {
-            //if we want to log more stuff
-            //Log::addEntry("HTTP request of validation request:". curl_getinfo($ch, CURLINFO_HEADER_OUT) ." for IPN payload: $req");
-            //Log::addEntry("HTTP response of validation request: $res");
-            curl_close($ch);
-        }
-        // Inspect IPN validation result and act accordingly
-        // Split response headers and payload, a better way for strcmp
-        $tokens = explode("\r\n\r\n", trim($res));
-        $res = trim(end($tokens));
-        if (strcmp ($res, "VERIFIED") == 0) {
+            $res = curl_exec($ch);
+            if (curl_errno($ch) != 0) // cURL error
+            {
+                Log::addError("Can't connect to PayPal to validate IPN message: " . curl_error($ch));
+                curl_close($ch);
+                exit;
+            } else {
+                //if we want to log more stuff
+                //Log::addEntry("HTTP request of validation request:". curl_getinfo($ch, CURLINFO_HEADER_OUT) ." for IPN payload: $req");
+                //Log::addEntry("HTTP response of validation request: $res");
+                curl_close($ch);
+            }
+            // Inspect IPN validation result and act accordingly
+            // Split response headers and payload, a better way for strcmp
+            $tokens = explode("\r\n\r\n", trim($res));
+            $res = trim(end($tokens));
+            if (strcmp($res, "VERIFIED") == 0) {
 
-            if (isset($_POST['invoice']) && $_POST['invoice']) {
                 $order = StoreOrder::getByID($_POST['invoice']);
                 if ($order) {
-                    $order->completeOrder($_POST['txn_id']);
-                    $order->updateStatus(StoreOrderStatus::getStartingStatus()->getHandle());
+                    if ($_POST['payment_status'] == 'Completed' || $_POST['payment_status'] == 'Canceled_Reversal') {
+                        $order->completeOrder($_POST['txn_id']);
+                        $order->updateStatus(StoreOrderStatus::getStartingStatus()->getHandle());
+                    }
+
+                    if ($_POST['payment_status'] == 'Refunded' || $_POST['payment_status'] == 'Reversed') {
+                        $order->setRefunded(new \DateTime());
+                        $order->setRefundReason($_POST['reason_code']);
+                        $order->save();
+                    }
+
                 } else {
                     Log::addWarning("IPN Warning: Order " . $_POST['invoice'] . " not found");
                 }
-            }
 
-        } elseif (strcmp ($res, "INVALID") == 0) {
-            // log for manual investigation
-            // Add business logic here which deals with invalid IPN messages
-            Log::addError("Invalid IPN: $req");
+            } elseif (strcmp($res, "INVALID") == 0) {
+                // log for manual investigation
+                // Add business logic here which deals with invalid IPN messages
+                Log::addError("Invalid IPN: $req");
+            }
         }
     }
 
